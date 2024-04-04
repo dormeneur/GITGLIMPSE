f982eba0b65412acd32ed1fea230342684f99643
Aditya Bharti <92594389+Cold-Coffee-Coder@users.noreply.github.com>
2024-04-04 13:14:49 +0530

    almost there

--- src/code.py
+++ src/code.py
@@ -1,37 +1,30 @@
 import os
 import time
-from openai import OpenAI
 import requests
+from openai import OpenAI
 from dotenv import load_dotenv
 
-
 load_dotenv()
-# openai.api_key = os.getenv("OPENAI_API_KEY")
 github_token = os.getenv("GITHUB_TOKEN")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-
-client = OpenAI(
-  api_key=os.environ['OPENAI_API_KEY']
-)
+client = OpenAI(api_key = openai_api_key)
 
 def analyze_repository(repo_url):
     headers = {"Authorization": f"token {github_token}"}
-
     time.sleep(2)
-
     max_retries = 3
     initial_delay = 1
     endpoints = repo_url.split("/")[-2] + "/" + repo_url.split("/")[-1]
 
-
     for attempt in range(max_retries):
-        
         response = requests.get(f"https://api.github.com/repos/{endpoints}", headers=headers)
-
         if response.status_code == 200:
             repo_data = response.json()
+            #print(repo_data)
             if "full_name" in repo_data:
                 name = repo_data["full_name"]
+                #print("name: ", name)
             else:
                 print("Error: Could not find repository name in response data.")
                 return None
@@ -47,11 +40,10 @@ def analyze_repository(repo_url):
             try:
                 response = client.completions.create(model = "davinci-002", prompt = prompt)
             except:
-                print("\nEither of the two things happend:\n  1. You don't have sufficient quota for this model today.\n  2. You're broke and don't have access to this GPT text model.\n...and that's why I can't complete a single project\n")
+                print("\nEither of the two things happend:\n  1. You don't have sufficient quota for this model today.\n  2. You're broke and don't have access to this GPT text model.\n...and that's why I can't even complete a single project\n")
                 exit()
 
             analysis = response.choices[0].text.strip()
-
             return analysis
 
         elif response.status_code == 403:
@@ -66,7 +58,6 @@ def analyze_repository(repo_url):
 if __name__ == "__main__":
 
     repo_url = input("Enter the GitHub repository URL: ")
-
     analysis = analyze_repository(repo_url)
 
     if analysis:
